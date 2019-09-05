a4d8fc625151ed4a73be17f6facea278ecc3e100
nojeffrey <macca728@hotmail.com>
2019-09-05 14:27:55 +1000

    updated links

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/open?id=1dzG8bYbeUUzIAH1rKxhkjEuWxUEwRSJu'
+export_file_name = 'WillVsChad_export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['chad', 'will']
 path = Path(__file__).parent
 
 app = Starlette()
